85b05d4c1a5ac7f1aa49529b2e3f4de7e5612c1a
Jordan Wilhite <jordanwilhite@gmail.com>
2015-07-16 14:15:37 -0500

    Added the initial setup for the $.ajax project

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the README file for the ajax project due Monday July 20th. 
---
+++ main.js
@@ -0,0 +1,10 @@
+// $.ajax({
+//   url:'https://api.github.com/users/jordanwilhite';
+// });
+//
+// $.ajax({
+//   url: 'https://api.github.com/users/jordanwilhite/repos'
+// });
+
+var $result = $('.results');
+var url = 'https://api.github.com/users/jordanwilhite';
