84df0291a73be9f1a28e6254e495a490f8c3f152
Jordan Wilhite <jordanwilhite@gmail.com>
2015-07-16 15:17:45 -0500

    second commit after I logged github urls

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-// $.ajax({
-//   url:'https://api.github.com/users/jordanwilhite';
-// });
-//
-// $.ajax({
-//   url: 'https://api.github.com/users/jordanwilhite/repos'
-// });
-
 var $result = $('.results');
 var url = 'https://api.github.com/users/jordanwilhite';
+var repos = 'https://api.github.com/users/jordanwilhite/repos';
+
+console.log($.ajax(url,{}));
+console.log($.ajax(repos,{}));
