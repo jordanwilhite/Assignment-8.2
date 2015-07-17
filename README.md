d616d71c42ebcb3b27c1eef8a47292e6b78dc3c2
Jordan Wilhite <jordanwilhite@gmail.com>
2015-07-17 11:07:35 -0500

    added a bunch of stuffthat loads without errors

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-This is the README file for the ajax project due Monday July 20th. 
+This is the README file for the ajax project due Monday July 20th.
+
+Note: When refreshing the API, You'll get an error after 'x' number of refreshes. 
--- main.js
+++ main.js
@@ -1,6 +1,74 @@
-var $result = $('.results');
-var url = 'https://api.github.com/users/jordanwilhite';
+var $main = $('.main');
+var $sidebar =('.sidebar')
+var $result = $('.result');
+var mainurl = 'https://api.github.com/users/jordanwilhite';
 var repos = 'https://api.github.com/users/jordanwilhite/repos';
+// var error = "error"; Take this out, maybe?
 
-console.log($.ajax(url,{}));
-console.log($.ajax(repos,{}));
+var access_token = "bf2f63910395da2a11db7e7d6b1c3b30f04237eb"
+$.ajax({
+   url: "https://api.github.com/users/jordanwilhite/repos",
+   method: "GET",
+   data: {
+     access_token: access_token
+   },
+
+ })
+
+ var access_token = "bf2f63910395da2a11db7e7d6b1c3b30f04237eb"
+$.ajax({
+   url: "https://api.github.com/users/jordanwilhite/repos",
+   method: "GET",
+   data: {
+     access_token: access_token
+   },
+
+ })
+
+var displayProfile = function(data, textStatus, xhr){
+$.ajax(mainurl,{
+  error: function(){
+    $result.text('Sorry, bro ' + error);
+  },
+  success: function(data){
+    var profile = data;
+    var name = profile.name;
+    var blog = profile.blog;
+    var location = profile.location;
+    var email = profile.email;
+    var avatar = profile.avatar_url;
+    var html = profile.html_url;
+    var repositories = profile.repositories;
+
+
+    $('.results .name').text(name);
+    $('.results .blog').text(blog);
+    $('.results .location').text(location);
+    $('.results .email').text(email);
+    $('.results .avatar').text(avatar);
+    $('.results .html').text(html);
+
+// console.log(mainurl);
+
+    }
+
+  });
+};
+
+var errorDisplayingProfile = function(xhr, type, error){
+    $result.text("Oooops! " + error);
+};
+
+
+var displayProfile = function(data, textStatus, xhr){
+$.ajax(repos,{
+  error: function(){
+    $result.text('Sorry, bro ' + error);
+
+  },
+});
+};
+
+var errorDisplayingProfile = function(xhr, type, error){
+    $result.text("Oooops! " + error);
+};
