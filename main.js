07726e657ac01f66d0660e21fd3d86a45be71696
Jordan Wilhite <jordanwilhite@gmail.com>
2015-07-21 22:51:32 -0500

    updated margins on main.js

--- main.js
+++ main.js
@@ -1,51 +1,56 @@
 var $main = $('.main');
 var $repos =('.repos')
-// var $result = $('.result');
 var mainurl = 'https://api.github.com/users/jordanwilhite';
 var repos = 'https://api.github.com/users/jordanwilhite/repos';
 
 
 var access_token = "bf2f63910395da2a11db7e7d6b1c3b30f04237eb"
-$.ajax({
-   url: "https://api.github.com/users/jordanwilhite",
-   method: "GET",
-   data: {
-     access_token: access_token
-   },
+    $.ajax({
+      url: "https://api.github.com/users/jordanwilhite",
+      method: "GET",
+      data: {
+      access_token: access_token
+            },
 
- })
+});
 
- var access_token = "bf2f63910395da2a11db7e7d6b1c3b30f04237eb"
-$.ajax({
-   url: "https://api.github.com/users/jordanwilhite/repos",
-   method: "GET",
-   data: {
-     access_token: access_token
-   },
+var access_token = "bf2f63910395da2a11db7e7d6b1c3b30f04237eb"
+    $.ajax({
+      url: "https://api.github.com/users/jordanwilhite/repos",
+      method: "GET",
+      data: {
+      access_token: access_token
+            },
 
- })
+});
 
-// var displayProfile = function(data, textStatus, xhr){
+
 $.ajax(mainurl,{
   error: function(xhr, type, error){
-    $main.text('Sorry ' + error);
+  $main.text('Sorry ' + error);
   },
+
   success: function(data, textStatus, xhr){
     var profile = data;
     var avatar = profile.avatar_url;
 
-    $img = $('<img>').attr('src', avatar);
-      $main.append($img);
-    $h3 = $('<h3>').text(profile.name);
-      $main.append($h3);
-    $h4 = $('<h4>').text(profile.email);
-       $main.append($h4);
-    $p = $('<p>').text(profile.html_url);
-      $main.append($p);
-    $blog = $('<blog>').text(profile.blog);
-      $main.append($blog);
-    $location = $('<location>').text(profile.location);
-      $main.append($location);
+      $img = $('<img>').attr('src', avatar);
+        $main.append($img);
+
+      $h3 = $('<h3>').text(profile.name);
+        $main.append($h3);
+
+      $h4 = $('<h4>').text(profile.email);
+        $main.append($h4);
+
+      $p = $('<p>').text(profile.html_url);
+        $main.append($p);
+
+      $blog = $('<blog>').text(profile.blog);
+        $main.append($blog);
+
+      $location = $('<location>').text(profile.location);
+        $main.append($location);
 
 
 
